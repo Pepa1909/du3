a58c6957644e3f5b9cac70b8b5b00b50443a9004
Josef Vojkovský <pepavoj@email.cz>
2021-12-10 00:51:13 +0100

    zkouška

--- du3_josef_vojkovsky.py
+++ du3_josef_vojkovsky.py
@@ -6,6 +6,6 @@ with open("adresy.geojson", encoding="utf-8") as file:
     souradnice_y = data["features"][0]["geometry"]["coordinates"][-1]
     ulice = data["features"][0]["properties"]["addr:street"]
     cislo_domu = data["features"][0]["properties"]["addr:housenumber"]
-    wgs2jtsk = Transformer.from_crs(4326,5514,always_xy=True)
+    wgs2jtsk = Transformer.from_crs(4326,5514, always_xy=True)
     out = wgs2jtsk.transform(souradnice_x, souradnice_y)
-    print(ulice, cislo_domu, out) 
+    print(ulice, cislo_domu, out)  
