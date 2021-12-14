99ad46fb060d2404d7bb768315e649317e2d77c0
Josef Vojkovský <pepavoj@email.cz>
2021-12-14 14:36:02 +0100

    pridany for cykly, skoro pocita vzdalenosti

--- du3_josef_vojkovsky.py
+++ du3_josef_vojkovsky.py
@@ -1,11 +1,24 @@
 from pyproj import Transformer
 import json
-with open("adresy.geojson", encoding="utf-8") as file:
-    data = json.load(file)
-    souradnice_x = data["features"][0]["geometry"]["coordinates"][0]
-    souradnice_y = data["features"][0]["geometry"]["coordinates"][-1]
-    ulice = data["features"][0]["properties"]["addr:street"]
-    cislo_domu = data["features"][0]["properties"]["addr:housenumber"]
+import math
+with open("adresy.geojson", encoding="utf-8") as adresy, open("kontejnery.geojson",encoding="utf-8") as kontejnery:
+    data_adresy = json.load(adresy)
+    souradnice = data_adresy["features"][0]["geometry"]["coordinates"]
+    ulice = data_adresy["features"][0]["properties"]["addr:street"]
+    cislo_domu = data_adresy["features"][0]["properties"]["addr:housenumber"]
     wgs2jtsk = Transformer.from_crs(4326,5514, always_xy=True)
-    out = wgs2jtsk.transform(souradnice_x, souradnice_y)
-    print(ulice, cislo_domu, out)  
+    out = wgs2jtsk.transform(*souradnice)
+    data_kontejnery = json.load(kontejnery) 
+    list_kont=[]
+    for kontejner in data_kontejnery["features"]:
+        list_kont.append(kontejner["geometry"]["coordinates"])
+    list_adres=[]
+    for adresa in data_adresy["features"]:
+        jtsk_adresa = wgs2jtsk.transform(*adresa["geometry"]["coordinates"])
+        list_adres.append(jtsk_adresa)
+    print(list_adres[0][0],list_adres[0][-1])
+    print(list_kont[0][0],list_kont[0][-1])
+        # vzdalenost_x = list_adres[0][0]-(list_kont[0][0])
+        # vzdalenost_y = list_adres[0][-1]-( list_kont[0][-1])
+        # print(math.sqrt(vzdalenost_x**2+vzdalenost_y**2))
+        # list_adres = []
