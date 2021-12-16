c207b25260b8772a0f9237ade8fe6413e10965fb
Josef Vojkovský <pepavoj@email.cz>
2021-12-16 17:34:50 +0100

    Issues vyřešeny - funkce jednodušší, v manuálu napsány požadavky

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## Uživatleský manuál
 
-Do složky k programu je nutné přiložit soubory "adresy.geojson", který lze stáhnout např. [zde](http://overpass-turbo.eu/) pro jakoukoli část Prahy.
-Druhým souborem, který je potřeba přiložit, je "kontejnery.geojson", který lze stáhnout na [stránkách Geoportálu Prahy](https://www.geoportalpraha.cz/cs/data/otevrena-data/8726EF0E-0834-463B-9E5F-FE09E62D73FB).
+Do složky k programu je nutné přiložit soubor "adresy.geojson", který lze stáhnout např. [zde](http://overpass-turbo.eu/) pro jakoukoli část Prahy. Tento soubor musí být v souřadnicovém systému WGS84 a souřadnice musí být v atributu `"coordinates"` v sekci `"geometry"`. Soubor musí obsahovat v atributu `"properties"` mít atributy `"addr:street"` a `"addr:housenumber"`.
+Druhým souborem, který je potřeba přiložit, je "kontejnery.geojson", který lze stáhnout na [stránkách Geoportálu Prahy](https://www.geoportalpraha.cz/cs/data/otevrena-data/8726EF0E-0834-463B-9E5F-FE09E62D73FB). Souřadnice musí být v JTSK systému v atributu `"geometry"` -> `"coordinates"`. Zároveň musí existovat atribut `"PRISTUP"`, který říká, jestli je kontejner veřejně přístupný nebo je jen pro obyvatele domu.
 
 Program po spuštení do obélníku dole vypíše:
 - kolik celkem adres a kontejnerů kontroloval
-- jaká je průměrná nejnižší vzdálenost ke koši na tříděný odpad pro danou část Prahy
-- z jaké adresy je to je koši na tříděný odpad nejdál a kolik je to metrů
-- medián nejkratších vzdáleností ke kontejnerům
+- jaká je průměrná nejnižší vzdálenost k veřejnému koši na tříděný odpad pro danou část Prahy
+- z jaké adresy je to k veřejnému koši na tříděný odpad nejdál a kolik je to metrů
+- medián nejkratších vzdáleností k veřejným kontejnerům
--- du3_josef_vojkovsky.py
+++ du3_josef_vojkovsky.py
@@ -1,4 +1,3 @@
-from json.decoder import JSONDecodeError
 from pyproj import Transformer
 import json
 import math
@@ -10,70 +9,35 @@ def vypocet_vzdalenosti(misto_jtsk, kontejner):
 
     vzdalenost_x = misto_jtsk[0]-(kontejner["geometry"]["coordinates"][0])
     vzdalenost_y = misto_jtsk[-1]-(kontejner["geometry"]["coordinates"][-1])
-    vzdalenost = math.sqrt(vzdalenost_x**2+vzdalenost_y**2)
+    vzdalenost = math.sqrt(vzdalenost_x**2 + vzdalenost_y**2)
     return vzdalenost
 
-def urceni_nejmensi_vzdalenosti(list_vzdalenosti,vzdalenost):
+def urceni_nejmensi_vzdalenosti(vzdalenost, min_dist):
     
-    """Přidá do seznamu novou vzdálenost a následně porovná s již existující vzdáleností.\n
-    Vrátí nejnižší vzdálenost adresy ke kontejneru na tříděný odpad"""
+    """Kontroluje nejnižší vzdálenost, kterou pak vrátí."""
 
-    list_vzdalenosti.append(vzdalenost)
-    if list_vzdalenosti[-1] < list_vzdalenosti[0]:
-        list_vzdalenosti.remove(list_vzdalenosti[0])
-    elif len(list_vzdalenosti) > 1:
-        list_vzdalenosti.remove(list_vzdalenosti[-1])
-    nejmensi_vzdalenost = list_vzdalenosti[0]
-    return nejmensi_vzdalenost
+    if min_dist == None:
+        min_dist = vzdalenost
+    if vzdalenost < min_dist:
+        min_dist = vzdalenost
+    return min_dist
 
 def kos_nejdal(nejdal, min_vzdalenost, misto, minima_vzdalenosti):
 
     """Vytváří seznam, ve kterém je adresa, odkud je to ke koši nejdál a zároveň vzdálenost adresy.\n
     Vrací seznam, který zbyde - v něm je největší vzdálenost i se správnou adresou."""
     
-    if nejdal==0:
-        nejdal=[min_vzdalenost, misto["properties"]["addr:street"],misto["properties"]["addr:housenumber"]]
+    if nejdal == None:
+        nejdal = [min_vzdalenost, misto["properties"]["addr:street"], misto["properties"]["addr:housenumber"]]
     elif nejdal[0] < minima_vzdalenosti[-1]:
-        nejdal = [minima_vzdalenosti[-1],misto["properties"]["addr:street"],misto["properties"]["addr:housenumber"]]
+        nejdal = [minima_vzdalenosti[-1], misto["properties"]["addr:street"], misto["properties"]["addr:housenumber"]]
     return nejdal
 
 try:
-    # Načtení souborů a vytvoření všech potřebných seznamů, slovníků, převodníku a proměnné.
-    with open("adresy.geojson", encoding="utf-8") as json_adresy, open("kontejnery.geojson",encoding="utf-8") as json_kontejnery:
+    # Načtení souborů a vytvoření potřebných seznamů
+    with open("adresy.geojson", encoding = "utf-8") as json_adresy, open("kontejnery.geojson", encoding = "utf-8") as json_kontejnery:
         data_adresy = json.load(json_adresy)
         data_kontejnery = json.load(json_kontejnery) 
-        wgs2jtsk_prevodnik = Transformer.from_crs(4326,5514, always_xy=True)
-        vzdalenosti = []
-        minima = []
-        max_vzdalenost_ke_kosi = 0
-
-        # Projede celý seznam adres, každou přetransforuje do JTSK
-        for adresa in data_adresy["features"]:
-            jtsk_adresa = wgs2jtsk_prevodnik.transform(*adresa["geometry"]["coordinates"])
-
-            # Pro každou adresu spočítá vzdálenost k nejbližšímu kontejneru na tříděný odpad, nejmenší uloží do seznamu 
-            for kontejner in data_kontejnery["features"]:
-                if kontejner["properties"]["PRISTUP"] == "volně":
-                    vzdalenost_mista_od_kontejneru = vypocet_vzdalenosti(jtsk_adresa,kontejner)
-                    nejmensi_vzdalenost = urceni_nejmensi_vzdalenosti(vzdalenosti, vzdalenost_mista_od_kontejneru)
-                    if nejmensi_vzdalenost > 10000:
-                        print("Nejbližší kontejner k jedné z adres se nachází dále než 10 km, v datech bude chyba.")
-                        quit()
-            minima.append(nejmensi_vzdalenost) # Seznam minima obsahuje vzdálenosti nejbližších košů
-
-            # Určení adresy, ze které je to ke koši nejdál, příprava seznamu na další adresu
-            max_vzdalenost_ke_kosi = kos_nejdal(max_vzdalenost_ke_kosi, nejmensi_vzdalenost, adresa, minima) 
-            vzdalenosti=[]
-
-        # Vytištění výstupu   
-        print(f'''
-Načteno {len(data_adresy["features"])} adresních bodů.
-Načteno {len(data_kontejnery["features"])} kontejnerů na tříděný odpad.
-        
-Průměrná vzdálenost ke kontejneru je {statistics.mean(minima):.0f} m.
-Nejdále je to z adresy {max_vzdalenost_ke_kosi[-2]} {max_vzdalenost_ke_kosi[-1]}, vzdálenost je {max(minima):.0f} m. 
-Medián vzdáleností je {statistics.median(minima):.0f} m.
-''')
 
 # Výjimky, které program umí poznat
 except FileNotFoundError as filenotfound:
@@ -82,6 +46,44 @@ except FileNotFoundError as filenotfound:
 except PermissionError as permission:
     print(f"Program nemá oprávnění ke čtení souboru {permission.filename}.")
     quit()
-except JSONDecodeError:
+except json.decoder.JSONDecodeError:
     print(f"Minimálně jeden ze souborů je prazdný.")
     quit()
+
+# Vytvoření převodníku, seznamu na nejbližší koše a konstanty na nejvzdálenější koš
+wgs2jtsk_prevodnik = Transformer.from_crs(4326,5514, always_xy = True)
+minima = []
+MAX_VZDALENOST_KE_KOSI = None
+
+# Projede celý seznam adres, každou přetransformuje do JTSK
+for adresa in data_adresy["features"]:
+    jtsk_adresa = wgs2jtsk_prevodnik.transform(*adresa["geometry"]["coordinates"])
+
+    # Toto platí pro první kontejner počítaný z adresy
+    nejmensi_vzdalenost = None
+    
+    # Pro každou adresu spočítá vzdálenost k nejbližšímu kontejneru na tříděný odpad, nejmenší uloží do seznamu 
+    for kontejner in data_kontejnery["features"]:
+        if kontejner["properties"]["PRISTUP"] == "volně":
+            vzdalenost_mista_od_kontejneru = vypocet_vzdalenosti(jtsk_adresa, kontejner)
+            nejmensi_vzdalenost = urceni_nejmensi_vzdalenosti(vzdalenost_mista_od_kontejneru, nejmensi_vzdalenost)   
+            
+    minima.append(nejmensi_vzdalenost) # Seznam minima obsahuje vzdálenosti nejbližších košů
+
+    # Když je nové nejmenší číslo větší než 10 km, program se ukončí
+    if minima[-1] > 10000:
+        print("Nejbližší kontejner k jedné z adres se nachází dále než 10 km, v datech bude chyba.")
+        quit()
+
+    # Určení adresy, ze které je to ke koši nejdál
+    MAX_VZDALENOST_KE_KOSI = kos_nejdal(MAX_VZDALENOST_KE_KOSI, nejmensi_vzdalenost, adresa, minima) 
+
+# Vytištění výstupu   
+print(f'''
+Načteno {len(data_adresy["features"])} adresních bodů.
+Načteno {len(data_kontejnery["features"])} kontejnerů na tříděný odpad.
+
+Průměrná vzdálenost ke kontejneru je {statistics.mean(minima):.0f} m.
+Nejdále je to z adresy {MAX_VZDALENOST_KE_KOSI[-2]} {MAX_VZDALENOST_KE_KOSI[-1]}, vzdálenost je {max(minima):.0f} m. 
+Medián vzdáleností je {statistics.median(minima):.0f} m.
+''')
